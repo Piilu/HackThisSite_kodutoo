de5c1ed7c9930d4c3fdba45a6b1e87aa292f10ff
Piilu <piilrainer@gmail.com>
2022-06-11 11:30:43 +0300

    task2

---
+++ task2/task2.js
@@ -0,0 +1,71 @@
+let getPixels = require("get-pixels")
+var abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?!.,;:+-/=';
+var morsecode =[
+    '.-',
+    '-...',
+    '-.-.',
+    '-..',
+    '.',
+    '..-.',
+    '--.',
+    '....',
+    '..',
+    '.---',
+    '-.-',
+    '.-..',
+    '--',
+    '-.',
+    '---',
+    '.--.',
+    '--.-',
+    '.-.',
+    '...',
+    '-',
+    '..-',
+    '...-',
+    '.--',
+    '-..-',
+    '-.--',
+    '--..',
+    '.----',
+    '..---',
+    '...--',
+    '....-',
+    '.....',
+    '-....',
+    '--...',
+    '---..',
+    '----.',
+    '-----',
+    '..--..',
+    '-.-.--',
+    '.-.-.-',
+    '--..--',
+    '-.-.-.',
+    '---...',
+    '.-.-.',
+    '-....-',
+    '-..-.',
+    '-...-',
+
+  ];
+getPixels("./pic.png", function(err, pixels) {
+  if(err) {
+    console.log("Bad image path")
+    return
+  }
+  let pixelArray=[];
+  for(let i=0;i<pixels.data.length;i+=4){
+    let pixel = pixels.data.slice(i,i+4);
+    pixelArray.push(pixel);
+  }
+  let split = pixelArray.map(p=>p[0]==0?0:1).join("").split("1");
+  let nums = split.map(s=> s.length+1);
+  nums.pop();
+  let morse = nums.map(num=>String.fromCharCode(num)).join("");
+  let answer = morse.split(" ").map(m=>{
+    let index = morsecode.indexOf(m)
+    return abc[index];
+  });
+  console.log(answer.slice(0,-1).join("").toLowerCase());
+})
